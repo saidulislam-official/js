10c88d65d97b8e5eddf5702c9c2f6168966b943f
saidulislam-official <saidulislam.own@gmail.com>
2024-01-06 09:32:25 +0600

    added variable

---
+++ math.js
@@ -0,0 +1,55 @@
+/*
+    1. number : 55, 88, -32
+    2. integer : 2, 5
+    3. float : 3.14, 1.2
+    4. operator : +, -, *, /
+    5. operation : addition, subtraction, multiplication, division
+    6. percent : 100%
+    7. equal : =
+    8. average : 3.5
+    9. remainder : 5 % 2 = 1
+    10. power : 2 ** 3 = 8
+    11. increment : 1++
+    12. decrement : 1--
+    13. infinity : 1/0 = infinity
+    14. NaN : 0/0 = NaN
+    15. Math : Math.PI, Math.random(), Math.round(), Math.ceil(), Math.floor(), Math.sqrt(), Math.abs(), Math.pow(), Math.max(), Math.min(), Math.sin(), Math.cos(), Math.tan(), Math.asin(), Math.acos(), Math.atan(), Math.log(), Math.exp(), Math.E, Math.LN2, Math.LN10, Math.LOG2E, Math.LOG10E, Math.PI, Math.SQRT1_2, Math.SQRT2
+    16. comparison : >, <, >=, <=, ==, ===, !=, !==
+    17. max,min,abs,round,ceil,floor,random,log,sqrt,pow,sin,cos,tan,asin,acos,atan,exp,PI,E,LOG2E,LOG10E,LN2,LN10,SQRT1_2,SQRT2
+    18. sequence : 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    19. series : 2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+    20. odd : 1, 3, 5, 7, 9
+    21. even : 2, 4, 6, 8, 10
+    22. prime : 2, 3, 5, 7
+    23. composite : 4, 6, 8, 9
+    24. factorial : 5! = 5*4*3*2*1 = 120
+    25. permutation : nPr = n!/(n-r)!
+    26. combination : nCr = n!/(n-r)!*r!
+    27. arithmetic progression : 2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+    28. geometric progression : 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024
+    29. harmonic progression : 1/2, 1/4, 1/8, 1/16, 1/32, 1/64, 1/128, 1/256, 1/512, 1/1024
+    30. sum of n terms : n(n+1)/2
+    31. sum of n odd terms : n^2
+    32. sum of n even terms : n(n+1)
+    33. sum of n terms of an AP : n/2(2a+(n-1)d)
+    34. sum of n terms of a GP : a(r^n-1)/r-1
+    35. sum of n terms of a HP : n/2a+(n-1)d
+    36. area of a circle : PI*r^2
+    37. circumference of a circle : 2*PI*r
+    38. area of a triangle : 1/2*b*h
+    39. perimeter of a triangle : a+b+c
+    40. area of a rectangle : l*w
+    41. perimeter of a rectangle : 2(l+w)
+    42. area of a square : a^2
+    43. perimeter of a square : 4a
+    :::: Always remember that, in JavaScript, the first index is 0. ::::
+    :::: Always remember that, in JavaScript, the last index is n-1. ::::
+    :::: Always remember that, in JavaScript, the length of an array is n. ::::
+
+    ***------------------------------------***
+
+    ::: Always Learning Mindset :::
+
+    ***------------------------------------***
+
+*/
---
+++ variable.js
@@ -0,0 +1,15 @@
+// variable declaration
+
+// numeric
+var age = 25;
+var temperature = 37;
+var price = 550.45;
+var number = 55;
+
+// string
+var name = "Rahim";
+var location = 'Dhaka';
+
+// boolean
+var isHot = false;
+var isRich = true;
