76e34c8de415504322c399b8273b4764075bd6c8
saidulislam-official <saidulislam.own@gmail.com>
2024-01-06 16:00:24 +0600

    added array

---
+++ array.js
@@ -0,0 +1,11 @@
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var age = [25, 30, 35, 40, 45];
+
+var tourPlaces = ['Cox\'s Bazar', 'Sajek', 'Sreemangal', 'Sundarban'];
+var tourFee = [5000, 6000, 7000, 8000];
+
+var friends = ['Rahim', 'Karim', 'Jabbar', 'Barkat'];
+
+console.log(tourPlaces);
+
+console.log(friends.length);
