788ba1b4daf2bdf540ff18c72426a5e3e248b416
saidulislam-official <saidulislam.own@gmail.com>
2024-01-14 20:33:58 +0600

    added stingr problem 1

---
+++ js-string-problems-practice/case.js
@@ -0,0 +1,14 @@
+const userName =  'blackPink';
+const userInput =  'blackPink';
+
+console.log(userName.toLowerCase());
+console.log(userInput.toUpperCase());
+
+/*
+if (userName === userInput) {
+    console.log('user exists');
+}
+else {  
+    console.log('user not found');
+}
+*/
